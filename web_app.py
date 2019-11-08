6097a7649dec442e615d446f74dc81cd3b24f6fe
Paras <spunater@yahoo.com>
2019-11-08 16:40:53 -0500

    Fixed link to U.K. news

--- web_app.py
+++ web_app.py
@@ -18,7 +18,7 @@ def index():
     world = news.world_news()
     return render_template('home.html', political = political[0], political_link = political[1], tech = tech[0], tech_link = tech[1],
                     business = business[0], business_link = business[1], US_CA = US_CA[0], US_CA_link = US_CA[1], ME = ME[0],
-                    ME_link = ME[1], EU = EU[0], EU_link = EU[1], UK = UK[0], UK_link = [1], Asia = Asia[0], Asia_link = Asia[1],
+                    ME_link = ME[1], EU = EU[0], EU_link = EU[1], UK = UK[0], UK_link = UK[1], Asia = Asia[0], Asia_link = Asia[1],
                     India = India[0], India_link = India[1], world = world[0], world_link = world[1])
 
 app.run()
